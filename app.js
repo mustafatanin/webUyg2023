a5e99f29a7d691226367ab9096cbf7b47b3c3f17
Mustafa <mustafataninn@gmail.com>
2023-11-09 14:31:31 +0300

    veritabanı bağlantısı oluşturuldu

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const db_connect=require('./db/mysql_connect')
+require('dotenv/config')
 const app = express()
 app.get('/app', function (req, res) {
-  res.send('Hello World')
+  res.send('Anasayfa')
 })
+app.get('/app', function (req, res){
+    res.send('app sayfası')
+  })
 
-app.listen(3000)
+app.listen(process.env.PORT)
