5f9f04a300cac6840ee4bc4d368fe09b4c2f2d0e
Mustafa <mustafataninn@gmail.com>
2023-11-09 15:16:54 +0300

    login router tasarlandı

---
+++ routers/routes.js
@@ -0,0 +1,8 @@
+const router=require('express').Router()
+const {login,register}=require('../controllers/controller')
+router.post("/login",login)
+
+router.post("/register",register)
+//post:veri gönderme get:veri alma put:veri güncelleme patch:veri güncelleme
+//delete:veri silme
+module.exports=router
